1fc48194a265ce6eb35ca398b7178323ba664a3a
phuctaile <tai.le1@tyme.com>
2020-01-04 23:33:04 +0700

    fix ci

---
+++ build/precache-manifest.e9a8cb4c3c4b13ec0143644db5e1d09c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be460d8dbcf0fb1467b9eab8909304a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6311327cfb1b4a1c21c6",
+    "url": "/static/css/main.72ac0b0b.chunk.css"
+  },
+  {
+    "revision": "62e6cd3b181efae4f73b",
+    "url": "/static/js/2.edd26d4b.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/static/js/2.edd26d4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6311327cfb1b4a1c21c6",
+    "url": "/static/js/main.64d9a0af.chunk.js"
+  },
+  {
+    "revision": "272c1ff85882e1362db2",
+    "url": "/static/js/runtime-main.1a84057b.js"
+  }
+]);
