2457bb3dcc28b42c7fb76d04d8533d69f473e270
phuctaile <tai.le1@tyme.com>
2020-06-24 20:52:14 +0700

    update

--- src/controller.js
+++ src/controller.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 80;
 app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/build/index.html'));
-  });
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 app.get('/getPdf', (req, res) => {
     const { url } = req.query;
